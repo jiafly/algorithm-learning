1690c24ccedffc0a8cfd0ae1e1ecc9977c50c5fd
jiafly <liuyi1181@163.com>
2021-03-17 11:12:03 +0800

    first commit

---
+++ src/main/java/com/jiafly/algorithm/offer/Offer07BuildTree.java
@@ -0,0 +1,58 @@
+package com.jiafly.algorithm.offer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 【题目】剑指 Offer 07. 重建二叉树
+ * <p>
+ * 【描述】输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * <p>
+ * 【限制】0 <= 节点个数 <= 5000
+ * <p>
+ * 【示例】
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 返回如下的二叉树：
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ *
+ * @author liuyi
+ */
+public class Offer07BuildTree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private Map<Integer, Integer> indexes = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = inorder.length;
+        for (int i = 0; i < n; ++i) {
+            indexes.put(inorder[i], i);
+        }
+        return build(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+    private TreeNode build(int[] preorder, int[] inorder, int p1, int p2, int i1, int i2) {
+        if (p1 > p2 || i1 > i2) {
+            return null;
+        }
+        int rootVal = preorder[p1];
+        int pos = indexes.get(rootVal);
+        TreeNode node = new TreeNode(rootVal);
+        node.left = pos == i1 ? null : build(preorder, inorder, p1 + 1, pos - i1 + p1, i1, pos - 1);
+        node.right = pos == i2 ? null : build(preorder, inorder, pos - i1 + p1 + 1, p2, pos + 1, i2);
+        return node;
+    }
+}
